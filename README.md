adcd7d6da8a3b2acd0deab1c19b5d9db05a602b0
aspiringguru <bmatthewtaylor@gmail.com>
2017-09-03 15:37:29 +1000

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# reactNativeReduxDemo2
---
+++ index.android.js
@@ -0,0 +1,9 @@
+// Index.ios.js - place code in here for IOS!!!!
+
+// Import a library to help create a component
+import React from 'react';
+import { AppRegistry } from 'react-native';
+import App from './src/app';
+
+// Render it to the device
+AppRegistry.registerComponent('auth', () => App);
